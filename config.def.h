bec32272cc2b555cdb4beb11922fc76510b71ef7
flaport <floris.laporte@gmail.com>
2019-06-06 17:27:26 +0200

    systray patch

--- config.def.h
+++ config.def.h
@@ -1,12 +1,16 @@
 /* See LICENSE file for copyright and license details. */
 
 /* master monitor */
-static const unsigned int MASTERMON  = 4;        /* master monitor [1-...] */
+static const unsigned int MASTERMON  = 3;        /* master monitor [1-...] */
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 20;       /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
