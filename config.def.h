b27f45625455f419eb060f281f59fcbe5c18ee9c
flaport <floris.laporte@gmail.com>
2019-07-04 19:20:14 +0200

    instances with instance name 'float' will automatically be floating

--- config.def.h
+++ config.def.h
@@ -55,7 +55,8 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ NULL,     NULL,       "Figure",       0,            1,           -1 },
+	{ NULL,       NULL,      "Figure",    0,            1,           -1 },
+	{ NULL,      "float",     NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
