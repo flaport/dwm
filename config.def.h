83e15b010ddf16a70444507c3fbdff95e2f13eaa
flaport <floris.laporte@gmail.com>
2019-11-03 14:20:15 +0100

    toggle bar mod+b --> mod+shift+b

--- config.def.h
+++ config.def.h
@@ -86,7 +86,7 @@ static const Layout layouts[] = {
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
