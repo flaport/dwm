08bda0be12cbbb528cbd5d663a6cc83eead57ee3
flaport <floris.laporte@gmail.com>
2020-04-15 19:17:57 +0200

    added recommended fonts for statusbar

--- config.def.h
+++ config.def.h
@@ -6,7 +6,16 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = {
+    "monospace:pixelsize=14:antialias=true:autohint=true",
+	"Symbola:pixelsize=12:antialias=true:autohint=true",
+	"Pomodoro:pixelsize=12:antialias=true:autohint=true",
+	"FontAwesome:pixelsize=12:antialias=true:autohint=true",
+	"Octicons:pixelsize=12:antialias=true:autohint=true",
+	"Icomoon:pixelsize=12:antialias=true:autohint=true",
+	"Hack Nerd Font:pixelsize=12:antialias=true:autohint=true",
+	"PowerlineSymbols:pixelsize=12:antialias=true:autohint=true",
+};
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
