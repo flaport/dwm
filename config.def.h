a53994c20d40664823ae55d783b6d7becf3ea8a6
flaport <floris.laporte@gmail.com>
2020-04-20 22:59:20 +0200

    reduce snapping

--- config.def.h
+++ config.def.h
@@ -3,8 +3,8 @@
 /* appearance */
 static const unsigned int stextdelim = 32;      /* ascii code of delimiter character used in status bar */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 20;       /* gap pixel between windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappx     = 16;       /* gap pixel between windows */
+static const unsigned int snap      = 16;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
