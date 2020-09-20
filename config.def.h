6f6e4ca7269b4be9126859da2aa7281505e5d895
flaport <floris.laporte@gmail.com>
2020-09-20 12:43:47 +0200

    make alacritty default terminal

--- config.def.h
+++ config.def.h
@@ -110,7 +110,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
