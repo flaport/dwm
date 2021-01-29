490f76136fc1c76c64121c646381f1b82ef4ab81
ciacconas <ciacconas@gmail.com>
2021-01-29 14:50:21 +0100

    Lumerical python api window noswallow

--- config.def.h
+++ config.def.h
@@ -77,6 +77,7 @@ static const Rule rules[] = {
 	{ NULL, NULL, "Customize PyCharm",            0,          1,         1,          0,          0,      -1 }, /* PyCharm */
 	{ NULL, NULL, "Event Tester",                 0,          1,         1,          0,          1,      -1 }, /* xev */
 	{ NULL, NULL, "Figure",                       0,          1,         1,          0,          1,      -1 }, /* Matplotlib */
+	{ NULL, NULL, "Lumerical",                    0,          1,         0,          0,          0,      -1 }, /* Lumerical */
 	{ NULL, NULL, "Import PyCharm Settings",      0,          1,         1,          0,          0,      -1 }, /* PyCharm */
 	{ NULL, NULL, "License Activation",           0,          1,         1,          0,          0,      -1 }, /* PyCharm */
 	{ NULL, NULL, "Marker Database Browser",      0,          1,         1,          0,          0,      -1 }, /* KLayout */
